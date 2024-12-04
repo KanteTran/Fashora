75d762157719258ee4df65fbbae9752dcaf81efe
Dante <tran@192.168.1.2.non-exists.ptr.local>
2024-12-05 01:30:30 +0700

    ref: Update function

--- run/start.sh
+++ run/start.sh
@@ -3,4 +3,4 @@
 echo "Starting script.sh with nohup..."
 nohup go run app/main.go > app/app.log 2>&1 &
 echo $! > app/app.pid
-echo "script.sh is running in the background with PID: $(cat app.pid)"
+echo "script.sh is running in the background with PID: $(cat app/app.pid)"
